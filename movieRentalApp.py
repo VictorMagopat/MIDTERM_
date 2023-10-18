77246586ea54db4ee0c7b3732992e948996f378f
VictorMagopat <magopat.victor@gmail.com>
2023-10-18 13:55:52 -0400

    final

--- movieRentalApp.py
+++ movieRentalApp.py
@@ -2,12 +2,16 @@ import movieStore
 
 def Run():
     print("Movie Rental Store - Victor Magopat" )
+    moviestore_1 = movieStore.MovieStore
+    moviestore_1.Set_Name()
+    moviestore_1.Set_Total_Movies()
+    moviestore_1.Set_Avalible_Movies()
+    moviestore_1.Set_rental_Fee()
+    moviestore_1.Set_Late_Fee()
+    moviestore_1.get_Name(moviestore_1)
+    moviestore_1.get_Total_Movies(moviestore_1)
+    moviestore_1.get_Avalible_Movies(moviestore_1)
+    moviestore_1.get_rental_Fee(moviestore_1)
+    moviestore_1.get_Late_Fee(moviestore_1)
 
-
-Film_store_1 = movieStore.MovieStore
-Film_store_1.Store_Name = "MISSISSAUGA"
-Film_store_1.Num_Movies_Total = 1
-Film_store_1.Num_Movies_Ava = 1
-Film_store_1.Rental_Fee = 5
-Film_store_1.Late_Fee = 10
-
+Run()
