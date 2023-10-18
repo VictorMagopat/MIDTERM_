1802e81b8b9d15f1d3230ac3ee97a436adfece0b
VictorMagopat <magopat.victor@gmail.com>
2023-10-18 12:39:41 -0400

    MovieStore constructor created

--- movieRentalApp.py
+++ movieRentalApp.py
@@ -2,12 +2,14 @@ import movieStore
 
 def Run():
     print("Movie Rental Store - Victor Magopat" )
-
+    FlimZ = movieStore.MovieStore
 
 FlimZ = movieStore.MovieStore
 FlimZ.Store_Name = "MISSISSAUGA"
-FlimZ.Num_Movies = 1
+FlimZ.Num_Movies_Total = 1
+FlimZ.Num_Movies_Ava = 1
 FlimZ.Rental_Fee = 5
 FlimZ.Late_Fee = 10
 
-print(FlimZ.Store_Name, FlimZ.Num_Movies, FlimZ.Rental_Fee, FlimZ.Late_Fee)
+
+print(FlimZ.Store_Name, FlimZ.Num_Movies_Total, FlimZ.Num_Movies_Ava, FlimZ.Rental_Fee, FlimZ.Late_Fee)
--- movieStore.py
+++ movieStore.py
@@ -2,9 +2,10 @@
 
 
 class MovieStore:
-    def __init__(self, Store_Name, Num_Movies, Rental_Fee, Late_Fee):
+    def __init__(self, Store_Name, Num_Movies_Total, Num_Movies_Ava, Rental_Fee, Late_Fee):
         self.Store_Name = Store_Name
-        self.Num_Movies = Num_Movies
+        self.Num_Movies_Total = Num_Movies_Total
+        self.Num_Movies_Ava = Num_Movies_Ava
         self.Rental_Fee = Rental_Fee
         self.Late_Fee = Late_Fee
 
