0852b745ee722f524ffa7dfef682c32da3ec988e
VictorMagopat <magopat.victor@gmail.com>
2023-10-18 12:29:48 -0400

    Class Is Working
    Made a class

--- movieRentalApp.py
+++ movieRentalApp.py
@@ -1,1 +1,13 @@
-print("it works")
+import movieStore
+
+def Run():
+    print("Movie Rental Store - Victor Magopat" )
+
+
+FlimZ = movieStore.MovieStore
+FlimZ.Store_Name = "MISSISSAUGA"
+FlimZ.Num_Movies = 1
+FlimZ.Rental_Fee = 5
+FlimZ.Late_Fee = 10
+
+print(FlimZ.Store_Name, FlimZ.Num_Movies, FlimZ.Rental_Fee, FlimZ.Late_Fee)
--- movieStore.py
+++ movieStore.py
@@ -1,1 +1,11 @@
-print("it work")
+
+
+
+class MovieStore:
+    def __init__(self, Store_Name, Num_Movies, Rental_Fee, Late_Fee):
+        self.Store_Name = Store_Name
+        self.Num_Movies = Num_Movies
+        self.Rental_Fee = Rental_Fee
+        self.Late_Fee = Late_Fee
+
+
